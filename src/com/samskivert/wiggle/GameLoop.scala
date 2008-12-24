9683d9ffe19b3d2b64285e319b392668f48871a4
mdb <mdb@85d40ae6-5935-11de-992f-fdd7e25b9163>
2008-12-24 22:34:20 +0000

    The start of an experiment in 2D graphics with Scala and LWJGL.

---
+++ src/com/samskivert/wiggle/GameLoop.scala
@@ -0,0 +1,99 @@
+//
+// $Id$
+
+package com.samskivert.wiggle
+
+import org.lwjgl.LWJGLException
+import org.lwjgl.input.Keyboard
+import org.lwjgl.opengl._
+import org.lwjgl.util.Timer
+
+/**
+ * Handles the main game loop.
+ */
+class GameLoop (config :DisplayConfig)
+{
+  def run () {
+    init
+
+    while (_running) {
+      Display.update // update the display
+      Timer.tick // update the Timer system
+
+      if (Display.isCloseRequested) {
+        stop
+
+      } else if (Display.isActive) {
+        logic
+        render
+        Display.sync(config.framerate)
+
+      } else {
+        // our window is not active, sleep for a bit to consume less CPU
+        try {
+          Thread.sleep(100)
+        } catch {
+          case e :Exception => Console.println("Sleep failed " + e)
+        }
+
+        logic
+        if (Display.isVisible || Display.isDirty) render
+      }
+    }
+  }
+
+  def addEntity (entity :Entity) {
+    _entities = entity :: _entities
+  }
+
+  def stop () {
+    _running = false
+  }
+
+  protected def init () {
+    try {
+      Display.setTitle(config.title)
+      Display.setFullscreen(false)
+      Display.setVSyncEnabled(true)
+      Display.setDisplayMode(new DisplayMode(config.width, config.height))
+      Display.create // start by creating the display
+      _running = true
+
+      // set the background to black
+      GL11.glClearColor(0, 0, 0, 0)
+
+      // enable textures since we're going to use these for our sprites
+      GL11.glEnable(GL11.GL_TEXTURE_2D)
+
+      // disable the OpenGL depth test since we're rendering 2D graphics
+      GL11.glDisable(GL11.GL_DEPTH_TEST)
+
+      // set up ortho rendering mode and the canvas size
+      GL11.glMatrixMode(GL11.GL_PROJECTION)
+      GL11.glLoadIdentity()
+      GL11.glOrtho(0, config.width, config.height, 0, -1, 1)
+
+    } catch {
+      case e :LWJGLException => e.printStackTrace(System.err)
+    }
+  }
+
+  protected def logic () {
+    if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) stop
+    _entities.foreach(_.logic(_timer.getTime))
+  }
+
+  protected def render () {
+    // clear the screen
+    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT)
+    GL11.glMatrixMode(GL11.GL_MODELVIEW)
+    GL11.glLoadIdentity()
+
+    // render our entities
+    _entities.foreach(_.render(_timer.getTime))
+  }
+
+  protected var _running = false
+  protected var _timer = new Timer
+  protected var _entities :List[Entity] = Nil
+}
---
+++ src/com/samskivert/wiggle/Wiggle.scala
@@ -0,0 +1,43 @@
+//
+// $Id$
+
+package com.samskivert.wiggle
+
+import org.lwjgl.opengl.GL11
+
+/**
+ * Does something extraordinary.
+ */
+object Wiggle
+{
+  def main (args :Array[String]) {
+    val config = DisplayConfig("Hello world!", 60, 800, 600)
+    var loop :GameLoop = new GameLoop(config)
+
+    loop.addEntity(new Element with Entity {
+      x = config.width/2
+      y = config.height/2
+
+      override def logic (time :Float) {
+        orient += 4.0f % 360; // rotate the square
+      }
+
+      override def renderElement (time :Float) {
+        GL11.glBegin(GL11.GL_QUADS)
+    	GL11.glColor3f(1, 0, 1)
+        GL11.glVertex2i(-50, -50)
+    	GL11.glColor3f(1, 1, 0)
+        GL11.glVertex2i(50, -50)
+    	GL11.glColor3f(0, 1, 1)
+        GL11.glVertex2i(50, 50)
+    	GL11.glColor3f(1, 1, 1)
+        GL11.glVertex2i(-50, 50)
+        GL11.glEnd()
+      }
+    })
+
+    loop.run
+  }
+
+  protected val FRAMERATE = 60
+}
