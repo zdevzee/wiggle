398e643e364187b1156971e5af3296e76ca69ec4
mdb <mdb@85d40ae6-5935-11de-992f-fdd7e25b9163>
2008-12-25 01:07:53 +0000

    Added Groups, handle Elements and Entities properly in GameLoop.

--- src/main/wiggle/test/Wiggle.scala
+++ src/main/wiggle/test/Wiggle.scala
@@ -8,6 +8,7 @@ import org.lwjgl.opengl.GL11
 import wiggle.game.DisplayConfig
 import wiggle.game.GameLoop
 import wiggle.gfx.Element
+import wiggle.gfx.Group
 import wiggle.sim.Entity
 
 /**
@@ -15,31 +16,33 @@ import wiggle.sim.Entity
  */
 object Wiggle
 {
+  class Square(sx :Int, sy :Int) extends Element {
+    x = sx
+    y = sy
+
+    override def renderElement (time :Float) {
+      orient += 4.0f % 360; // rotate the square
+
+      GL11.glBegin(GL11.GL_QUADS)
+      GL11.glColor3f(1, 0, 1)
+      GL11.glVertex2i(-50, -50)
+      GL11.glColor3f(1, 1, 0)
+      GL11.glVertex2i(50, -50)
+      GL11.glColor3f(0, 1, 1)
+      GL11.glVertex2i(50, 50)
+      GL11.glColor3f(1, 1, 1)
+      GL11.glVertex2i(-50, 50)
+      GL11.glEnd()
+    }
+  }
+
   def main (args :Array[String]) {
     val config = DisplayConfig("Hello world!", 60, 800, 600)
     var loop :GameLoop = new GameLoop(config)
 
-    loop.addEntity(new Element with Entity {
-      x = config.width/2
-      y = config.height/2
-
-      override def logic (time :Float) {
-        orient += 4.0f % 360; // rotate the square
-      }
-
-      override def renderElement (time :Float) {
-        GL11.glBegin(GL11.GL_QUADS)
-    	GL11.glColor3f(1, 0, 1)
-        GL11.glVertex2i(-50, -50)
-    	GL11.glColor3f(1, 1, 0)
-        GL11.glVertex2i(50, -50)
-    	GL11.glColor3f(0, 1, 1)
-        GL11.glVertex2i(50, 50)
-    	GL11.glColor3f(1, 1, 1)
-        GL11.glVertex2i(-50, 50)
-        GL11.glEnd()
-      }
-    })
+    val group = new Group
+    for (x <- 0.to(800).by(100) ; y <- 0.to(600).by(100)) group.add(new Square(x, y))
+    loop.add(group)
 
     loop.run
   }
