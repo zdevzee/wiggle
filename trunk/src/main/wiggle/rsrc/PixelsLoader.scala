b9396a48bee9fc4d4d259999e02258644ee723b1
mdb <mdb@85d40ae6-5935-11de-992f-fdd7e25b9163>
2008-12-27 22:53:09 +0000

    Pixels loading. Untested.

--- trunk/src/main/wiggle/rsrc/PixelsLoader.scala
+++ trunk/src/main/wiggle/rsrc/PixelsLoader.scala
@@ -3,28 +3,98 @@
 
 package wiggle.rsrc
 
+import java.awt.Color
+import java.awt.Transparency
+import java.awt.color.ColorSpace
+import java.awt.image.BufferedImage
+import java.awt.image.ComponentColorModel
+import java.awt.image.DataBuffer
+import java.awt.image.DataBufferByte
+import java.awt.image.Raster
+import javax.imageio.ImageIO
+
+import org.lwjgl.BufferUtils
+
 /**
  * Loads image data and prepares it for use as textures.
  */
 class PixelsLoader (rldr :ResourceLoader)
-  extends Function1[String, Pixels]
 {
   /** Loads and returns the pixels with the specified path.
+   * @param config specifies how to convert the image data when loading.
    * @throws NoSuchElementException if the pixels resource could not be found.
    */
-  def get (path :String) = loadPixels(rldr.get(path))
+  def get (path :String, config :PixelsConfig) = loadPixels(rldr.get(path), config)
 
-  /** Locates and returns an option on the specified pixels. */
-  def getOption (path :String) = rldr.getOption(path) match {
-    case Some(rsrc) => loadPixels(rsrc)
+  /** Locates and returns an option on the specified pixels.
+   * @param config specifies how to convert the image data when loading.
+   */
+  def getOption (path :String, config :PixelsConfig) = rldr.getOption(path) match {
+    case Some(rsrc) => loadPixels(rsrc, config)
     case None => None
   }
 
-  /** Treats the pixels loader as a function from path to Pixels.
-   * @see #get */
-  def apply (path :String) = get(path)
+  /** Loads and converts image data. */
+  protected def loadPixels (rsrc :Resource, config :PixelsConfig) :Pixels = {
+    val image = rsrc.asFile match {
+      case Some(file) => ImageIO.read(file)
+      case None => ImageIO.read(rsrc.asStream)
+    }
 
-  protected def loadPixels (rsrc :Resource) :Pixels = {
-    throw new UnsupportedOperationException("TODO")
+    // compute a texture size that is a power of two in both dimensions
+    val texWidth = nextPowTwo(image.getWidth, 2)
+    val texHeight = nextPowTwo(image.getHeight, 2)
+    val hasAlpha = image.getColorModel.hasAlpha || config.forceAlpha
+
+    // for now we support only 24 or 32 bit depth
+    val bands = if (hasAlpha) 4 else 3
+    val raster = Raster.createInterleavedRaster(
+      DataBuffer.TYPE_BYTE, texWidth, texHeight, bands, null)
+    val cmodel = if (hasAlpha) GlAlphaColorModel else GlColorModel
+    val texImage = new BufferedImage(cmodel, raster, false, null)
+
+    // draw the original image into the target image (which converts it to the appropriate format
+    // for OpenGL) optionally flipping it in the process
+    val gfx = texImage.createGraphics
+    try {
+      gfx.setColor(Blank)
+      gfx.fillRect(0, 0, texWidth, texHeight)
+      if (config.flipped) {
+        gfx.scale(1, -1)
+        gfx.drawImage(image, 0, -image.getHeight, null)
+      } else {
+        gfx.drawImage(image, 0, 0, null)
+      }
+    } finally {
+      gfx.dispose()
+    }
+
+    // extract the raw image data from the target image's raster
+    val bytes = texImage.getRaster.getDataBuffer.asInstanceOf[DataBufferByte].getData
+    val data = BufferUtils.createByteBuffer(bytes.length)
+    data.put(bytes, 0, bytes.length)
+    data.flip()
+
+    new Pixels(image.getWidth, image.getHeight, bands * BitsPerByte, texWidth, texHeight, data)
   }
+
+  /** Helper function for computing the next higher power of two for a value. */
+  protected def nextPowTwo (value :Int, current :Int) :Int =
+    if (current > value) current else nextPowTwo(value, current*2)
+
+  /** A blank color used as the default pixel for texture image data. */
+  protected val Blank = new Color(0f, 0f, 0f, 0f)
+
+  /** The color model used for images with an alpha-channel. */
+  protected val GlAlphaColorModel = new ComponentColorModel(
+    ColorSpace.getInstance(ColorSpace.CS_sRGB), Array(8, 8, 8, 8), true, false,
+    Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+
+  /** The color model used for images without an alpha-channel. */
+  protected val GlColorModel = new ComponentColorModel(
+    ColorSpace.getInstance(ColorSpace.CS_sRGB), Array(8, 8, 8, 0), false, false,
+    Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+
+  /** The number of bits per byte. For great magic number avoidance. */
+  protected val BitsPerByte = 8
 }
