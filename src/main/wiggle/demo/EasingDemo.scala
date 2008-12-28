0cd5f9d19eee4b6f0964fae50b8715660ee75fa5
mdb <mdb@85d40ae6-5935-11de-992f-fdd7e25b9163>
2008-12-28 02:24:51 +0000

    A basic Color class as well as a class for constructing and rendering OpenGL primitives that uses interleaved arrays.

--- src/main/wiggle/demo/EasingDemo.scala
+++ src/main/wiggle/demo/EasingDemo.scala
@@ -8,8 +8,10 @@ import org.lwjgl.opengl.GL11
 import wiggle.game.DisplayConfig
 import wiggle.game.Entity
 import wiggle.game.GameLoop
+import wiggle.gfx.Color
 import wiggle.gfx.Element
 import wiggle.gfx.Group
+import wiggle.gfx.Primitive
 import wiggle.util.Interpolator
 import wiggle.util.Task
 import wiggle.util.Taskable
@@ -19,20 +21,11 @@ import wiggle.util.Taskable
  */
 object EasingDemo
 {
-  class Square extends Element {
-    override def renderElement (time :Float) {
-      GL11.glBegin(GL11.GL_QUADS)
-      GL11.glColor3f(1, 0, 1)
-      GL11.glVertex2i(-50, -50)
-      GL11.glColor3f(1, 1, 0)
-      GL11.glVertex2i(50, -50)
-      GL11.glColor3f(0, 1, 1)
-      GL11.glVertex2i(50, 50)
-      GL11.glColor3f(1, 1, 1)
-      GL11.glVertex2i(-50, 50)
-      GL11.glEnd()
-    }
-  }
+  def makeSquare = Primitive.makeColorVertex(4).
+    color(Color.Red).vertex(-50, -50).
+    color(Color.Green).vertex(50, -50).
+    color(Color.Blue).vertex(50, 50).
+    color(Color.White).vertex(-50, 50).buildQuads
 
   def main (args :Array[String]) {
     val config = DisplayConfig("Easing Demo", 60, 800, 600)
@@ -40,18 +33,19 @@ object EasingDemo
 
     val group = new Group with Entity
     for (x <- 0.to(800).by(100) ; y <- 0.to(600).by(100); if (y != 300)) {
-      var square = new Square
+      var square = makeSquare
       square.move(x, y)
       group.add(square)
     }
     loop.add(group)
 
-    val square = new Square with Entity
-    square.move(0, config.height/2)
-    square.add(square.orientM.linear(360, 2).set(0).repeat)
-    square.add(square.xM.easeInOut(config.width/2, 1).delay(1).easeInOut(config.width, 1).
-               easeIn(config.width/2, 1).easeOut(0, 1).repeat)
-    loop.add(square)
+    val mover = new Group with Entity
+    mover.add(makeSquare)
+    mover.move(0, config.height/2)
+    mover.add(mover.orientM.linear(360, 2).set(0).repeat)
+    mover.add(mover.xM.easeInOut(config.width/2, 1).delay(1).easeInOut(config.width, 1).
+              easeIn(config.width/2, 1).easeOut(0, 1).repeat)
+    loop.add(mover)
     loop.run
   }
 }
