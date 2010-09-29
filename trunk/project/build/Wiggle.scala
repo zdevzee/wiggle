95f4b88fe86361ab8218b6888e698776730f85eb
samskivert <samskivert@85d40ae6-5935-11de-992f-fdd7e25b9163>
2010-09-29 00:19:59 +0000

    More build updates. Can now build and run the demos.

--- trunk/project/build/Wiggle.scala
+++ trunk/project/build/Wiggle.scala
@@ -1,3 +1,4 @@
+import java.io.File
 import sbt._
 
 class Coreen (info :ProjectInfo) extends DefaultProject(info) {
@@ -5,4 +6,16 @@ class Coreen (info :ProjectInfo) extends DefaultProject(info) {
   // val lwjgl = "org.lwjgl" % "lwjgl" % "2.1.0"
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
 
+  override def fork = Some(new ForkScalaRun {
+    val os = System.getProperty("os.name").split(" ")(0).toLowerCase match {
+      case "linux" => "linux"
+      case "mac" => "macosx"
+      case "windows" => "win32"
+      case x => error("Unsupported platform " + x)
+    }
+    override def runJVMOptions = super.runJVMOptions ++ Seq(
+      "-Djava.library.path=" + System.getProperty("java.library.path") + File.pathSeparator +
+      ("lib" / "native" / os))
+    override def scalaJars = Seq(buildLibraryJar.asFile, buildCompilerJar.asFile)
+  })
 }
